97e57e9544e51dba1936a1072e06b16ee6fbe2a6
sharon zheng <sharzheng@gmail.com>
2017-10-08 23:41:38 -0400

    adds zodiac sign onto about page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-const bday_icon = document.querySelector('div._4bl9._zu9 > ul li > div > div._4bl7 > div > i.sx_ecb85f');
+const bday_icon = document.querySelector('._36d div._4bl9._zu9 > ul li > div > div._4bl7 > div > i.sx_ecb85f');
+
 if (bday_icon) {
-  const bday = bday_icon.parentNode.parentNode.parentNode.querySelector('div:nth-child(2) > span > div:nth-child(2)').textContent;
+  const bday = bday_icon.parentNode.parentNode.parentNode.querySelector('div:nth-child(2) > span > div:nth-child(2)');
 
   const zodiac_signs = {
     1: [19, 'Capricorn'],
@@ -18,7 +19,7 @@ if (bday_icon) {
   };
 
   const rgx = /([JFMASOND].*[a-z]) (\d{0,2})/;
-  const [full, month, day] = bday.match(rgx);
+  const [full, month, day] = bday.textContent.match(rgx);
 
   let month_num = new Date(Date.parse(full)).getMonth() + 1;
   if (day > zodiac_signs[month_num][0]) { //if not part of that month's sign
@@ -26,4 +27,31 @@ if (bday_icon) {
   }
 
   const sign = zodiac_signs[month_num][1];
+  const overview_items = document.querySelector('._36d div._4bl9._zu9 > ul').querySelectorAll('li._4tnv._2pif');
+
+  const zodiac_html = `
+    <li class="_4tnv _2pif">
+      <div class="clearfix _ikh">
+        <div class="_4bl7">
+          <div class="_5pmc">
+            <i class="img sp_zR425wRe-ty_2x sx_ecb85f" id="zod"></i>
+          </div>
+        </div>
+        <div class="_4bl9 _2pis _2dbl">
+          <span class="_c24 _50f3">
+            <div><span class="accessible_elem">Zodiac Sign</span></div>
+            <div>${sign}</div>
+          </span>
+        </div>
+      </div>
+    </li>
+  `;
+
+  let zodiac_item = document.createElement('li');
+  zodiac_item.className = '_4tnv _2pif';
+  zodiac_item.innerHTML = zodiac_html;
+
+  const last_item = document.querySelector(`div._4bl9._zu9 > ul > li:nth-child(${overview_items.length})`);
+  last_item.appendChild(zodiac_item);
+  document.getElementById('zod').style.backgroundImage = `url("https://raw.githubusercontent.com/skinsshark/Astrologybook/master/signs/${sign.toLowerCase()}.png")`;
 }
